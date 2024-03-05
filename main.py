d377bfe8fb1fb9f7360209493a6882b6c5542d94
Alexandre Cabana <cabanaA@educ.college-montreal.qc.ca>
2024-03-05 12:37:41 -0500

    test with css to make table and display name with in template.

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
+templates = Jinja2Templates(directory="templates")
 app = FastAPI()
 
+app.mount("/css", StaticFiles(directory="css"), name="css")
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/testtemplatename/{name}")
+async def testtemplatename(request: Request, name: str):
+    return templates.TemplateResponse("testtemplatename.html", {"request":request, "name":name})
+
+dogs = [{"name":"Bob", "age":10},{"name":"Alice", "age":30},{"name":"David", "age":2}]
+@app.get("/testdisplaytable")
+async def testdisplaytable(request: Request):
+    return templates.TemplateResponse("testdisplaytable.html", {"request":request, "dogs":dogs})
